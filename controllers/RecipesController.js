b72fa7328e3d8dd38df634d4306d6326ced09702
AminaChakirDev <amina.aitm@gmail.com>
2021-05-06 23:31:57 +0200

    Create asyncErrorHandler method

--- controllers/RecipesController.js
+++ controllers/RecipesController.js
@@ -2,44 +2,26 @@ const { Recipe } = require("../models/Recipe");
 
 module.exports = {
 
-    getAll: async (req, res, next) => {
-        try {
-            res.json({ result: await Recipe.find() });
-        } catch (err) {
-            next(err);
-        }
+    getAll: async (req, res) => {
+        res.json({ result: await Recipe.find() });
     },
 
-    update: async (req, res, next) => {
-        try {
-            const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.recipeId, req.body, {new: true});
-            if (updatedRecipe === null) {
-                throw 'not found';
-            }
-            res.json({ result: updatedRecipe });
+    update: async (req, res) => {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.recipeId, req.body, {new: true});
+        if (updatedRecipe === null) {
+            throw 'not found';
         }
-        catch (err) {
-            next(err);
-        }
+        res.json({ result: updatedRecipe });
     },
 
-    delete: async (req, res, next) => {
-        try {
+    delete: async (req, res) => {
             await Recipe.deleteOne({_id: req.params.recipeId });
             res.json({ success: true });
-        } catch (err) {
-            next(err);
-        }
     },
 
-    create: async (req, res, next) => {
-        try {
-            await Recipe.init();
-            const newRecipe = new Recipe(req.body);
-            res.json({ success: true, result: await newRecipe.save() });
-        } 
-        catch (err) {
-            next(err);
-        }
+    create: async (req, res) => {
+        await Recipe.init();
+        const newRecipe = new Recipe(req.body);
+        res.json({ success: true, result: await newRecipe.save() });
     }
 };
--- server.js
+++ server.js
@@ -19,12 +19,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const asyncHandler = (controller) => {
-    return async (req, res, next) => {
+const asyncErrorHandler = (controller) => {
+    return async (req, res) => {
         try {
             await controller(req, res);
-        }
-        catch({ code, message, status }) {
+        } catch ({ code, message, status }) {
             res
                 .status(status || 500)
                 .json({
@@ -32,13 +31,13 @@ const asyncHandler = (controller) => {
                     message
                 });
         }
-    }
-}
+    };
+};
 
-app.get('/api/recipes', asyncHandler(RecipesController.getAll));
-app.post('/api/recipes', asyncHandler(RecipesController.create));
-app.put('/api/recipes/:recipeId', asyncHandler(RecipesController.update));
-app.delete('/api/recipes/:recipeId', asyncHandler(RecipesController.delete));
+app.get('/api/recipes', asyncErrorHandler(RecipesController.getAll));
+app.post('/api/recipes', asyncErrorHandler(RecipesController.create));
+app.put('/api/recipes/:recipeId', asyncErrorHandler(RecipesController.update));
+app.delete('/api/recipes/:recipeId', asyncErrorHandler(RecipesController.delete));
 
 app.use('*', (req, res) => {
     res.status(404).json({ error: "not found" });
