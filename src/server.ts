8bdd27218b3da4cefb89c412602673761d7f5b45
AminaChakirDev <amina.aitm@gmail.com>
2021-07-05 22:42:02 +0200

    Add Environmental Variables MONGO_URI

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
     await mongoose
-        .connect('mongodb://mongo:27017/instarecipes', {
+        .connect(process.env.MONGO_URI || 'mongodb://mongo:27017/instarecipes', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
